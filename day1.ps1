f036aec0f5b584d217a212c4feeee72343539673
thespanningtree <seth.mtl@outlook.com>
2021-03-07 10:50:42 -0500

    Day 1 complete

--- day1.ps1
+++ day1.ps1
@@ -1,1 +1,14 @@
-dir
+$directions = get-content C:\repo\adventofcode2015\input.txt
+$floor = 0
+foreach($character in $directions.ToCharArray()) 
+{ 
+   if   ($character -eq "(")
+   {
+        $floor = $floor + 1
+   }
+   else 
+   {
+        $floor = $floor - 1
+   }
+}
+   $floor
